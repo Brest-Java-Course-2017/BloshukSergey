45ea0786619fd86ed7c6eea4411cb5b9b19f73db
Sergey <mr-serj@outlook.com>
2017-03-18 15:53:30 +0300

    clean master

--- test-db/src/main/resources/sql/create-session-table.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IF EXISTS session;
-CREATE TABLE session (
-  session_id    INT           NOT NULL AUTO_INCREMENT,
-  movie_name    VARCHAR(30)   NOT NULL,
-  session_date  DATE          NOT NULL,
-  PRIMARY KEY (session_id)
-);
